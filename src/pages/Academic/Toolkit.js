6f64664695f4632bf88501babe75a6ba5f4ed3af
danielanapoli <dnapol01@gmail.com>
2025-08-12 18:05:42 -0400

    more changes to the toolkit page

--- src/pages/Academic/Toolkit.js
+++ src/pages/Academic/Toolkit.js
@@ -1,11 +1,14 @@
 import '../../styles/App.css';
 
 import { useState } from 'react';
+import { Link } from "react-router-dom";
+
 import Image from 'react-bootstrap/Image';
 import Container  from 'react-bootstrap/Container';
 import Carousel from 'react-bootstrap/Carousel';
-import { Link } from "react-router-dom";
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 import NavBar from '../../components/navbar';
 import Footer from '../../footer';
@@ -32,7 +35,7 @@ function Toolkit() {
                 <Breadcrumb.Item active>Design Toolkit</Breadcrumb.Item>
             </Breadcrumb>   
             <h1>Remote Healthcare Technology Design Toolkit</h1>
-            <Carousel data-bs-theme="dark" activeIndex={index} onSelect={handleSelect}>
+            <Carousel data-bs-theme="dark" activeIndex={index} onSelect={handleSelect} interval={1800}>
                 <Carousel.Item className='text-center'>
                     <Image src={`${process.env.PUBLIC_URL}/img/toolkit-instructions.png`} width={'80%'}/>
                 </Carousel.Item>
@@ -54,8 +57,9 @@ function Toolkit() {
             <p>
                 The toolkit has 15 useful UX tools, including:
                 <ul>
+                    <li><strong>How-To</strong> instructions to help decipher how the toolkit can help any team in UX</li>
                     <li><strong>Personas</strong> that highlight how different older adults have unique needs, privacy expectations, and tech opportunities.</li>
-                    <li><strong>Emapthy maps</strong>, with real quotes from older adults, unpacking their emotions, thoughts, and behaviours with remote healthcare technology.</li>
+                    <li><strong>Emapthy maps</strong> with real quotes from older adults, unpacking their emotions, thoughts, and behaviours with remote healthcare technology.</li>
                 </ul>
             </p>
             <h2>Data Behind the Tools</h2>
@@ -71,6 +75,23 @@ function Toolkit() {
             <p>
                 Check back soon for the full details on the toolkit including more information on how the toolkit was developed with the input from SMEs, and our future efforts to better shape remote healthcare technology something that older adults really need.
             </p>
+            <br/>
+            <br/>
+            <Row className="text-center">
+                <h5>In collaboration with...</h5>
+            </Row>
+            <br/>
+            <Row className="align-items-center">
+                <Col>                    
+                    <Image src={`${process.env.PUBLIC_URL}/img/carleton-logo.png`} width={'90%'} />
+                </Col>
+                <Col>  
+                    <Image src={`${process.env.PUBLIC_URL}/img/hc2p-logo.png`} width={'80%'} />
+                </Col>
+                <Col>  
+                    <Image src={`${process.env.PUBLIC_URL}/img/nrc-logo.png`} width={'50%'} />
+                </Col>
+            </Row>
         </Container>
       </section>    
       <Footer/>
