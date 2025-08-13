093552408489c20036fb137fb1129a719482cf85
danielanapoli <dnapol01@gmail.com>
2025-08-13 10:55:30 -0400

    Updates to the toolkit page

--- src/pages/Academic/Toolkit.js
+++ src/pages/Academic/Toolkit.js
@@ -33,53 +33,51 @@ function Toolkit() {
                 <Breadcrumb.Item href="/">Home</Breadcrumb.Item>
                 <Breadcrumb.Item href="/#/academic">Academic Portfolio</Breadcrumb.Item>
                 <Breadcrumb.Item active>Design Toolkit</Breadcrumb.Item>
-            </Breadcrumb>   
-            <br/><br/>
+            </Breadcrumb>
             <h1>Remote Healthcare Technology Design Toolkit</h1>
-            <Carousel data-bs-theme="dark" activeIndex={index} onSelect={handleSelect} interval={1800}>
-                <Carousel.Item className='text-center'>
-                    <Image src={`${process.env.PUBLIC_URL}/img/toolkit-instructions.png`} width={'80%'}/>
-                </Carousel.Item>
-                <Carousel.Item className='text-center'>
-                    <Image src={`${process.env.PUBLIC_URL}/img/toolkit-persona.png`} width={'80%'} />
-                </Carousel.Item>
-                <Carousel.Item className='text-center'>
-                    <Image src={`${process.env.PUBLIC_URL}/img/toolkit-empathymap.png`} width={'80%'} />
-                </Carousel.Item>
-            </Carousel>
             <br/>
-            <h2>Improving Remote Healthcare Tech</h2>
-            <p>
-                Communicating what users need in the well-being and aging space to those developing technology can be tough. 
-                This challenge is especially true when multidisciplinary teams are trying to find common ground among people with different expertise and priorities. 
-                The <strong>Remote Healthcare Technology Design Toolkit</strong> was made with two main goals in mind. First, to highlight what older adults need and expect in terms of privacy when it comes to remote healthcare technology. Second, to help teams dive into their users’ needs during the research or design process.
-            </p>
-            <h2>Toolkit Features</h2>
-            <p>
-                The toolkit has 15 useful UX tools, including:
-                <ul>
-                    <li><strong>How-To</strong> instructions to help decipher how the toolkit can help any team in UX</li>
-                    <li><strong>Personas</strong> that highlight how different older adults have unique needs, privacy expectations, and tech opportunities.</li>
-                    <li><strong>Emapthy maps</strong> with real quotes from older adults, unpacking their emotions, thoughts, and behaviours with remote healthcare technology.</li>
-                </ul>
-            </p>
-            <h2>Data Behind the Tools</h2>
-            <p>
-                The toolkit is built on rich survey and interview data (see <Link to="/#/academic/publications">Publications</Link>) and has been assessed through focus groups with professionals who know a lot about creating tech for health and older adults. 
-                Feedback from experts shows that our toolkit can help UX research, designers, and developers:
-                <ol>
-                    <li><strong>Build better understanding and empathy</strong> for the specific needs older adults have for remote healthcare tech.</li>
-                    <li><strong>Stay focused on those needs</strong> while brainstorming potential solutions tailored for this group.</li>
-                </ol>
-            </p>
-            <h2>In The Works</h2>
-            <p>
-                Check back soon for the full details on the toolkit including more information on how the toolkit was developed with the input from SMEs, and our future efforts to better shape remote healthcare technology something that older adults really need.
-            </p>
-            <br/>
-            <br/>
+            <Row fluid>
+                <Col md={7}>
+                    <h2>Improving Remote Healthcare Tech</h2>
+                    <p>
+                        Focusing on what users need while creating technology can be tough. This can be especially challenging while multidisciplinary teams, filled with people who have different expertise and priorities, are trying to find common ground.
+                    </p>
+                    <p>
+                        By highlighting what older adults need and expect in terms of privacy when it comes to the technology they need to manage their health and well-being,
+                        the toolkit can help teams dive into these needs when making solutions targeted to this demographic.
+                    </p>
+                    <h2>Features</h2>
+                    <p>
+                        The toolkit has 15 validated UX tools, including:
+                        <ul>
+                            <li><strong>How-Tos</strong> to help decipher how the toolkit can help anyone in UX</li>
+                            <li><strong>Personas</strong> to zero-in on how different older adults have unique needs, privacy expectations, and opportunities</li>
+                            <li><strong>Emapthy maps</strong> to unpack older adults' emotions, thoughts, and behaviours with remote healthcare tech</li>
+                        </ul>
+                    </p>
+                    <h2>Data-Driven Content</h2>
+                    <p>
+                        The toolkit is built on rich survey and interview data (see <Link to="/#/academic/publications">Publications</Link>) and it has been assessed by professionals experienced in healthcare technology and older adults. 
+                        Feedback from experts shows that our toolkit can help UX research, designers, and developers to...
+                    <ol>
+                        <li><strong>Build better understanding and empathy</strong> for the specific needs older adults have for remote healthcare tech.</li>
+                        <li><strong>Stay focused on those needs</strong> while brainstorming potential solutions tailored for this group.</li>
+                    </ol>
+                    </p>
+                    <h2>Coming Soon</h2>
+                    <p>
+                        Check back soon for the full details on the toolkit including more information on how the toolkit was developed with the input from SMEs, and our future efforts to better shape remote healthcare technology something that older adults really need.
+                    </p>
+                 </Col>
+                <Col md={5}>
+                    <Image src={`${process.env.PUBLIC_URL}/img/toolkit-instructions.png`} width={'100%'} className='pb-3'/>
+                    <Image src={`${process.env.PUBLIC_URL}/img/toolkit-persona.png`} width={'100%'} className='pb-3'/>
+                    <Image src={`${process.env.PUBLIC_URL}/img/toolkit-empathymap.png`} width={'100%'} className='pb-3'/>
+                </Col>
+            </Row> 
+            <br/><br/>        
             <Row className="text-center">
-                <h5>In collaboration with...</h5>
+                <h5><i>In collaboration with...</i></h5>
             </Row>
             <br/>
             <Row className="align-items-center">
